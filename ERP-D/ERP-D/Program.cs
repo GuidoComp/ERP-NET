2c41a5605b7c2942851d3d7e53996c4cdf23b9ac
Leandro Rodríguez <31992512@comunidadort.edu.ar>
2022-09-27 20:15:13 -0300

    Creación y configuración de clase StartUp

--- ERP-D/ERP-D/Program.cs
+++ ERP-D/ERP-D/Program.cs
@@ -1,36 +1,6 @@
-namespace ERP_D
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+using ERP_D;
 
-            var app = builder.Build();
+var app = StartUp.InicializarApp(args);            
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-    }
-}
+app.Run();
---
+++ ERP-D/ERP-D/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace ERP_D
+{
+    public static class StartUp
+    {
+        private static void ConfigureServices (WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews();
+        }
+
+        private static void Configure (WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+
+        public static WebApplication InicializarApp(String[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
+            var app = builder.Build();
+            Configure(app);
+            return app;
+        }
+    }
+}
