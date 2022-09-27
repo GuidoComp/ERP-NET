af2e97289cb2aebab0ee25a48c9e07b62694b182
Leandro Rodríguez <31992512@comunidadort.edu.ar>
2022-09-27 20:46:20 -0300

    Creación de ABM de CentrosDeCosto, Empleados y Personas. Agregamos los botones a mano en el LayOut porque no nos aparecían.

---
+++ ERP-D/ERP-D/Controllers/CentrosDeCostoController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ERP_D.Data;
+using ERP_D.Models;
+
+namespace ERP_D.Controllers
+{
+    public class CentrosDeCostoController : Controller
+    {
+        private readonly ErpContext _context;
+
+        public CentrosDeCostoController(ErpContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CentroDeCostos
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.CentrosDeCosto.ToListAsync());
+        }
+
+        // GET: CentroDeCostos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.CentrosDeCosto == null)
+            {
+                return NotFound();
+            }
+
+            var centroDeCosto = await _context.CentrosDeCosto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (centroDeCosto == null)
+            {
+                return NotFound();
+            }
+
+            return View(centroDeCosto);
+        }
+
+        // GET: CentroDeCostos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CentroDeCostos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,MontoMaximo")] CentroDeCosto centroDeCosto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(centroDeCosto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(centroDeCosto);
+        }
+
+        // GET: CentroDeCostos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.CentrosDeCosto == null)
+            {
+                return NotFound();
+            }
+
+            var centroDeCosto = await _context.CentrosDeCosto.FindAsync(id);
+            if (centroDeCosto == null)
+            {
+                return NotFound();
+            }
+            return View(centroDeCosto);
+        }
+
+        // POST: CentroDeCostos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,MontoMaximo")] CentroDeCosto centroDeCosto)
+        {
+            if (id != centroDeCosto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(centroDeCosto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CentroDeCostoExists(centroDeCosto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(centroDeCosto);
+        }
+
+        // GET: CentroDeCostos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.CentrosDeCosto == null)
+            {
+                return NotFound();
+            }
+
+            var centroDeCosto = await _context.CentrosDeCosto
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (centroDeCosto == null)
+            {
+                return NotFound();
+            }
+
+            return View(centroDeCosto);
+        }
+
+        // POST: CentroDeCostos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.CentrosDeCosto == null)
+            {
+                return Problem("Entity set 'ErpContext.CentrosDeCosto'  is null.");
+            }
+            var centroDeCosto = await _context.CentrosDeCosto.FindAsync(id);
+            if (centroDeCosto != null)
+            {
+                _context.CentrosDeCosto.Remove(centroDeCosto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CentroDeCostoExists(int id)
+        {
+          return _context.CentrosDeCosto.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ERP-D/ERP-D/Controllers/EmpleadosController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ERP_D.Data;
+using ERP_D.Models;
+
+namespace ERP_D.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private readonly ErpContext _context;
+
+        public EmpleadosController(ErpContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Empleados
+        public async Task<IActionResult> Index()
+        {
+            var erpContext = _context.Empleados.Include(e => e.Posicion);
+            return View(await erpContext.ToListAsync());
+        }
+
+        // GET: Empleados/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Empleados == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados
+                .Include(e => e.Posicion)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleado);
+        }
+
+        // GET: Empleados/Create
+        public IActionResult Create()
+        {
+            ViewData["PosicionId"] = new SelectList(_context.Posiciones, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Legajo,ObraSocial,EmpleadoActivo,PosicionId,Foto,Id,DNI,Nombre,Apellido,Email,Direccion,FechaAlta,UserName,Password")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(empleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PosicionId"] = new SelectList(_context.Posiciones, "Id", "Nombre", empleado.PosicionId);
+            return View(empleado);
+        }
+
+        // GET: Empleados/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Empleados == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+            ViewData["PosicionId"] = new SelectList(_context.Posiciones, "Id", "Nombre", empleado.PosicionId);
+            return View(empleado);
+        }
+
+        // POST: Empleados/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Legajo,ObraSocial,EmpleadoActivo,PosicionId,Foto,Id,DNI,Nombre,Apellido,Email,Direccion,FechaAlta,UserName,Password")] Empleado empleado)
+        {
+            if (id != empleado.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(empleado);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmpleadoExists(empleado.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PosicionId"] = new SelectList(_context.Posiciones, "Id", "Nombre", empleado.PosicionId);
+            return View(empleado);
+        }
+
+        // GET: Empleados/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Empleados == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados
+                .Include(e => e.Posicion)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleado);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Empleados == null)
+            {
+                return Problem("Entity set 'ErpContext.Empleados'  is null.");
+            }
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado != null)
+            {
+                _context.Empleados.Remove(empleado);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+          return _context.Empleados.Any(e => e.Id == id);
+        }
+    }
+}
